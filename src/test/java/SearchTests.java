2c6aed216b1eba68bb69fab7e95f4fbbcde509ef
t0l9 <t0l4ik.kas1@gmail.com>
2025-04-07 17:13:55 +0500

    add

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+
+public class SearchTests {
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://selenide.org"));
+    }
+
+    @Test
+    void pageCanBeOpenedTest() {
+        open("https://www.google.com/");
+        $("html").shouldHave(text("Србија"));
+    }
+}
